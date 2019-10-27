139680acd4ccbb27d5977e1aae9fc1257c792835
Sergi <smdt505@hotmail.com>
2019-10-27 16:49:21 +1000

    Adding validation to check if user has already logged in before

--- tinder-api.js
+++ tinder-api.js
@@ -17,23 +17,25 @@ const port = 80;
 // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
 app.listen(port, () => console.log('\x1b[31m%s\x1b[0m', `Tinder App listening on port ${port}`))
 
+// viewed at http://localhost:80
+app.get('/login', function(req, res) {
+    if(req.isAuthenticated()) {
+        console.log(req.user.token);
+        res.redirect('/tinder');
+    }
+    res.sendFile(path.join(__dirname + '/src/pages/login.html'));
+});
+
 app.get('/auth/facebook',
     passport.authenticate('facebook'));
 
 app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { successRedirect: '/tinder' }),
-    function(req, res) {
+    passport.authenticate('facebook', { successRedirect: '/tinder' }), function(req, res) {
     // Successful authentication, redirect home.
-        res.redirect('/tinder');
+    res.redirect('/tinder');
 });
 
-passport.authenticate('facebook', { successRedirect: '/',
-failureRedirect: '/login' }));
-
-// viewed at http://localhost:80
-app.get('/login', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/pages/login.html'));
-});
+passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' });
 
 // viewed at http://localhost:80
 app.get('/tinder', function(req, res) {
